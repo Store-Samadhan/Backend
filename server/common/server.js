f4d3e43f073e99f24d5102fe9b8d3a825661badb
HarshilMendpara <mendparaharshil@gmail.com>
2022-04-09 13:41:27 +0530

    Firebase Setup

--- server/common/server.js
+++ server/common/server.js
@@ -1,17 +1,14 @@
-import Express from 'express';
+import Express from "express";
 
+import cors from "cors";
 
-import cors from 'cors';
-
-import * as path from 'path';
-import * as bodyParser from 'body-parser';
-import * as http from 'http';
-import * as os from 'os';
-import l from './logger';
-import * as OpenApiValidator from 'express-openapi-validator';
-import errorHandler from '../api/middlewares/error.handler'
-
-
+import * as path from "path";
+import * as bodyParser from "body-parser";
+import * as http from "http";
+import * as os from "os";
+import l from "./logger";
+import * as OpenApiValidator from "express-openapi-validator";
+import errorHandler from "../api/middlewares/error.handler";
 
 const app = new Express();
 
@@ -19,19 +16,24 @@ export default class ExpressServer {
   constructor() {
     const root = path.normalize(`${__dirname}/../..`);
 
-    const apiSpec = path.join(__dirname, 'api.yml');
+    const apiSpec = path.join(__dirname, "api.yml");
     const validateResponses = !!(
       process.env.OPENAPI_ENABLE_RESPONSE_VALIDATION &&
-      process.env.OPENAPI_ENABLE_RESPONSE_VALIDATION.toLowerCase() === 'true'
+      process.env.OPENAPI_ENABLE_RESPONSE_VALIDATION.toLowerCase() === "true"
     );
 
-    app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || '100kb' }));
-    app.use(bodyParser.urlencoded({ extended: true, limit: process.env.REQUEST_LIMIT || '100kb' }));
-    app.use(bodyParser.text({ limit: process.env.REQUEST_LIMIT || '100kb'}));
+    app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || "100kb" }));
+    app.use(
+      bodyParser.urlencoded({
+        extended: true,
+        limit: process.env.REQUEST_LIMIT || "100kb",
+      })
+    );
+    app.use(bodyParser.text({ limit: process.env.REQUEST_LIMIT || "100kb" }));
 
     app.use(Express.static(`${root}/public`));
 
-    app.use(process.env.OPENAPI_SPEC || '/spec', Express.static(apiSpec));
+    app.use(process.env.OPENAPI_SPEC || "/spec", Express.static(apiSpec));
     app.use(
       OpenApiValidator.middleware({
         apiSpec,
@@ -40,29 +42,24 @@ export default class ExpressServer {
       })
     );
 
-
     app.use(cors());
-
   }
 
   router(routes) {
-
-    routes(app)
-    app.use(errorHandler)
+    routes(app);
+    app.use(errorHandler);
     return this;
-
   }
 
   listen(port = process.env.PORT) {
-    const welcome = p => () =>
+    const welcome = (p) => () =>
       l.info(
-        `up and running in ${process.env.NODE_ENV ||
-          'development'} @: ${os.hostname()} on port: ${p}}`
+        `up and running in ${
+          process.env.NODE_ENV || "development"
+        } @: ${os.hostname()} on port: ${p}}`
       );
 
-  
     http.createServer(app).listen(port, welcome(port));
-  
 
     return app;
   }
