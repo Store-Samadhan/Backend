1209a3166b71c5ea253beb0ff485ff3f69fc4c96
Akash Gupta <akash.gupta.anup@gmail.com>
2022-04-09 12:31:52 +0530

    Inital Backend Setup

---
+++ server/api/middlewares/error.handler.js
@@ -0,0 +1,7 @@
+
+// eslint-disable-next-line no-unused-vars, no-shadow
+export default function errorHandler(err, req, res, next) {
+  const errors = err.errors || [{ message: err.message }];
+  res.status(err.status || 500).json({ errors })
+}
+
